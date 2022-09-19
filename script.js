febc838b3513f5f28a4f030fb717092d86d8750e
jdyball11 <jack.dyball92@gmail.com>
2022-09-20 08:34:02 +1000

    reset button

--- script.js
+++ script.js
@@ -5,33 +5,87 @@
 const cells = document.querySelectorAll('.cell')
 const spanTurn = document.querySelector('span')
 const restartBtn = document.querySelector('#restart')
-console.log(restartBtn)
-console.log(spanTurn)
+const div = document.querySelector('div')
+
 const player1 = 'Player X Won!'
 const player2 = 'Player O Won!'
 let player = 1;
 
-//forEach loop is iterating over each child cell from cell variable declared earlier
-//event listener added to each cell so when the user clicks on one of the cells a conditional statement will run
-//player varibale is set to one and if this is true then we will fill in the cells space with the respective X or O
-//the span varibale text content will display the next players turn
-//then switch the player to 0 so on the next click it will run vice versa
 
+//iterate over each cell 
+// cells.forEach((cell) => {
+//     //when one of the cells are clicked, the below will run
+//     cell.addEventListener('click', (e) => {
+//         if (player === 1) {
+//             cell.innerHTML = 'X'
+//             spanTurn.textContent = "O's"
+//             player = 0;
+//         } else {
+//             cell.innerHTML = 'O'
+//             spanTurn.textContent = "X's"
+//             player = 1
+//         }
+//     })
+// })
+
+// const playerScore = []
+// const checkWinner = () => {
+//     cells.forEach((data) => {
+//         data.addEventListener('click', () => {
+//             console.log('clicked')
+//         })
+//     })
+// }
+
+
+const playerX = []
+const playerO = []
 
 cells.forEach((cell) => {
+    //when one of the cells are clicked, the below will run
     cell.addEventListener('click', (e) => {
+        if(cell.innerHTML != "") return;
+        console.log(cell.dataset.cell)
         if (player === 1) {
             cell.innerHTML = 'X'
+            playerX.push(cell.dataset.cell)
             spanTurn.textContent = "O's"
             player = 0;
         } else {
             cell.innerHTML = 'O'
+            playerO.push(cell.dataset.cell)
             spanTurn.textContent = "X's"
             player = 1
         }
+        
     })
+    
 })
 
-const checkWinner = () => {
 
-}
+//if cell 1 / 2/ 3 contain 'X' then player wins
+
+// const array = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [3, 6, 9],
+//     [3, 5, 7],
+//     [1, 5, 9]
+// ]
+
+console.log(cells)
+
+    restartBtn.addEventListener('click', (e) => {
+        
+        for (let remove of cells) {
+            remove.textContent = ''
+        }
+        
+    })
+
+
+
+

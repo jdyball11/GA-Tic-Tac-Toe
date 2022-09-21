823c77e94185ef637d454019fa967af830da523b
jdyball11 <jack.dyball92@gmail.com>
2022-09-21 15:50:10 +1000

    tie, display win & end game

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 
 
 const cells = document.querySelectorAll('.cell')
-const spanTurn = document.querySelector('span')
+const spanScoreTurn = document.querySelector('#player-position')
+const swanSpan = document.querySelector('#swan-score')
+const geelongSpan = document.querySelector('#geelong-score')
 const restartBtn = document.querySelector('#restart')
 const div = document.querySelector('div')
+const pointerRemove = document.querySelectorAll('.pointer')
 
-const playerXScore = 0
-const playerOScore = 0
+playerXStatus = false
+playerOStatus = false
+let playerX = []
+let playerO = []
+let playerXScore = 0
+let playerOScore = 0
 let player = 1;
+
 
 const winningCombo = [
     [0, 1, 2],
@@ -23,79 +31,118 @@ const winningCombo = [
 ]
 
 
-let playerX = []
-let playerO = []
-
-
+const startGame = () => {
 
 cells.forEach((cell) => {
     cell.addEventListener('click', (e) => {
         const dataSet = parseInt(e.target.dataset.cell)
-        
+        console.log(playerO)
+        console.log(playerX)
         if(cell.innerHTML != "") return;
     
             if (player === 1) {
                 cell.innerHTML = 'X'
                 playerX.push(dataSet)
-                spanTurn.textContent = "O's"
+                spanScoreTurn.textContent = "Player O's Turn"
+                checkForWinner(playerX)
+                checkForDraw()
                 player = 0;
-                winner(playerX)
+                // console.log(playerX)
             } else {
                 cell.innerHTML = 'O'
                 playerO.push(dataSet)
-                spanTurn.textContent = "X's"
+                spanScoreTurn.textContent = "Player X's Turn"
+                checkForWinner(playerO)
+                checkForDraw()
                 player = 1
-                winner(playerO)
+                
             }
-       
-    })
+            
+    });     
+    
     
 })
+}
+startGame()
+
+
 
 //when reset button is clicked loop will set text content to an empty string
+//will reset text in cells and remove pointer class from all cells
+//players arrays are reset to an empty array
+//Player is returned to player 1 so X starts
 
 restartBtn.addEventListener('click', (e) => {  
     //looping over every cell
     for (let remove of cells) {
         remove.textContent = ''
+        remove.classList.remove('pointer')
     }
-    spanTurn.textContent = ''
+    spanScoreTurn.textContent = ''
+    playerX = []
+    playerO = []
     player = 1
 })
 
 
 
-const winner = (player) => {
-  // Pull out each array
-winningCombo.forEach((e) => {
-    
-  // Check if player markings include any of the winning combos. Assign the boolean result to var result.
-  let result = e.every(element => {
-    
-    return player.includes(element)
-  })
-  // If they do, check the result var and return out of the forEach loop as soon as it finds one match.
-  if (result === true) {
-    console.log('winner')
-    return;
-  } 
-  })
-}
-
-
-
-
-const checkForDraw = () => {
-    cells.forEach((e) => {
-        if (e.textContent !== "") {
-            return true
-            
-        }
+const checkForWinner = (playerWinner) => {
+    // Pull out each array
+  winningCombo.forEach((e) => {
+      
+    // Check if player markings include any of the winning combos. Assign the boolean result to var result.
+    let result = e.every(element => {
+    return playerWinner.includes(element)
     })
+    // If they do, check the result var and return out of the forEach loop as soon as it finds one match.
+    if (result === true) {
+        if (player === 1) {
+            spanScoreTurn.textContent = 'Sydney Scored!'
+            playerXStatus === true
+            playerXScore++
+            console.log(playerXScore)
+            endGamePointer()
+           return
+        } else if (player === 0){
+            spanScoreTurn.textContent = 'Geelong Scored!'
+            playerOStatus === true
+            playerOScore++
+            console.log(playerOScore)
+            endGamePointer()
+            return
+        }
+    }  
+    })
+    playerScores(playerOScore, playerXScore)
+  }
+
+//if all cells are full, then a tie will be called
+const checkForDraw = () => {
+    let count = playerX.length + playerO.length
+    if (count === 9) {
+        console.log('tie')
+    }
+    playerOStatus = true
+    playerXStatus = true
+
 }
-checkForDraw()
 
+//increments team/player score
+const playerScores = (scoreO, scoreX) => {
+    if (scoreO) {
+        geelongSpan.textContent = scoreO
+    } else if (scoreX) {
+        swanSpan.textContent = scoreX
+    }
+}
 
+//prevents players from clicking on cells after winner is called
+const endGamePointer = () => {
+    cells.forEach((cell) => { 
+        cell.classList.add('pointer')
+    })     
+}
 
-
-       
+const grandFinalWinner = () => {
+    
+}
